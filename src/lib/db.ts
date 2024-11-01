cb09a81f104d979bed7c9b2c7fa699b5aae5e02d
Krishna Thakkar <work.krishnat@gmail.com>
2024-11-01 20:24:29 +0530

    .

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
+import { env } from "@/env";
+
+const createPrismaClient = () =>
+  new PrismaClient({
+    log:
+      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
